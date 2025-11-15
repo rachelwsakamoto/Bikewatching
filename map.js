81d4086003b13c37914d4a322cd51ca132a8f301
rachelwsakamoto <rwsakamoto@ucsd.edu>
2025-11-14 21:59:47 -0800

    Update map.js

--- map.js
+++ map.js
@@ -18,4 +18,13 @@ map.on('load', async () => {
   map.addSource('boston_route', {
   type: 'geojson',
   data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',});
+  map.addLayer({
+  id: 'bike-lanes',
+  type: 'line',
+  source: 'boston_route',
+  paint: {
+    'line-color': 'green',
+    'line-width': 3,
+    'line-opacity': 0.4,},
+});
 });
