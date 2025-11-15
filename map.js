e67edacc9e0bc78a7e171a973290e40f583dc8bf
rachelwsakamoto <rwsakamoto@ucsd.edu>
2025-11-14 21:58:59 -0800

    Update map.js

--- map.js
+++ map.js
@@ -12,3 +12,10 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+map.on('load', async () => {
+  //code
+  map.addSource('boston_route', {
+  type: 'geojson',
+  data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',});
+});
